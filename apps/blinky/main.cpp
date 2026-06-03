142221ba6b569db0ab3cb5ff5c45cce07cb22f7a
Tim Travica <tim.trvc@proton.me>
2026-06-03 22:02:52 +0200

    made blinky app a task

--- apps/blinky/main.cpp
+++ apps/blinky/main.cpp
@@ -2,12 +2,34 @@
 
 #include "led.hpp"
 
+class BlinkyTask {
+public:
+  void run(uint32_t current_tick) {
+    if (!is_initialized) {
+      Drivers::Led::init();
+      is_initialized = true;
+    }
+
+    if (current_tick - last_action_tick >= wait_ticks) {
+      Drivers::Led::toggle();
+      last_action_tick = current_tick;
+    }
+  }
+  
+private:
+  bool is_initialized = false;
+  uint32_t last_action_tick = 0;
+  uint32_t wait_ticks = 10000;
+};
+
+
 int main() {
-  Drivers::Led::init();
+  BlinkyTask blinky;
+
+  uint32_t system_tick = 0;
 
   while (true) {
-    Drivers::Led::toggle(); 
-    
-    Drivers::Led::delay(50000);
+    blinky.run(system_tick);
+    system_tick = system_tick + 1;
   }
 }
